fd000e984310f6414fce717a85911ce6023324de
Nikhil Berry <nikhilberry@gmail.com>
2021-10-07 09:40:38 -0700

    Functions Exercises

---
+++ project0/addCalc.py
@@ -0,0 +1,7 @@
+def addition_calc(num1, num2):
+    sum = num1 +num2
+    print("You added ", num1 , " and ", num2, " to get a sum of ", sum)
+
+addition_calc(3,5)
+addition_calc(4,20)
+addition_calc(100, 1000)
